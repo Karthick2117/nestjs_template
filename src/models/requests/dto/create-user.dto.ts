0e602671e397a4cefeb7b81abfa563fcae8ece3a
sam0rr <samor140@hotmail.com>
2025-09-22 22:53:13 -0400

    feat : fix swagger doc

--- src/models/requests/dto/create-user.dto.ts
+++ src/models/requests/dto/create-user.dto.ts
@@ -5,17 +5,21 @@ import {
   MaxLength,
   MinLength,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'bob dole', minLength: 2, maxLength: 50 })
   @IsString()
   @MinLength(2)
   @MaxLength(50)
   name!: string;
 
+  @ApiProperty({ example: 'bob@example.com', format: 'email', maxLength: 255 })
   @IsEmail()
   @MaxLength(255)
   email!: string;
 
+  @ApiProperty({ required: false, example: 'i am a bio', maxLength: 280 })
   @IsOptional()
   @IsString()
   @MaxLength(280)
