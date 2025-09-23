17c23ca6c1841e12a3def2d32360afc9e28ce13e
sam0rr <samor140@hotmail.com>
2025-09-22 22:28:38 -0400

    feat : main route to swagger is /swagger

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
     .setVersion('1.0.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  SwaggerModule.setup('swagger', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
