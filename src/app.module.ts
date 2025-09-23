c8ca583d50e01e4be70e980e12a3a931301d8f61
Samuel Arseneault <158058364+sam0rr@users.noreply.github.com>
2025-09-22 22:03:04 -0400

    docs: clarify how to extend features module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './services/app.service';
 import { DatabaseModule } from './modules/database/database.module';
-import { UsersModule } from './modules/users/users.module';
+import { FeaturesModule } from './modules/features/features.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [DatabaseModule, FeaturesModule],
   controllers: [AppController],
   providers: [AppService],
 })
